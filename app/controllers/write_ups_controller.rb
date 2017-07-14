dc4eafc38c52c66d60b8a1f74cac0edaa3f238a4
Nik <nerdvestor@gmail.com>
2017-07-14 12:13:41 -0700

    add cancan

--- app/controllers/write_ups_controller.rb
+++ app/controllers/write_ups_controller.rb
@@ -1,5 +1,4 @@
 class WriteUpsController < ApplicationController
-  load_and_authorize_resource
   before_action :authenticate_user!
   before_action :set_write_up, only: [:show, :edit, :update, :destroy]
 
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -63,9 +63,10 @@ ActiveAdmin.setup do |config|
   # ensure that there is a user with proper rights. You can use
   # CanCanAdapter or make your own. Please refer to documentation.
   # config.authorization_adapter = ActiveAdmin::CanCanAdapter
-  config.authorization_adapter = ActiveAdmin::CanCanAdapter  # In case you prefer Pundit over other solutions you can here pass
-  config.on_unauthorized_access = :access_denied
-  config.cancan_ability_class = "AdminAbility"
+  # config.authorization_adapter = ActiveAdmin::CanCanAdapter  # In case you prefer Pundit over other solutions you can here pass
+  # config.cancan_ability_class = "Ability"
+  # config.on_unauthorized_access = :access_denied
+  
 
   # the name of default policy class. This policy will be used in every
   # case when Pundit is unable to find suitable policy.

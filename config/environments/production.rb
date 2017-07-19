3d31994d1e4635193bbec7c9f950c6c505cfad88
Nik <nerdvestor@gmail.com>
2017-07-18 21:05:53 -0700

    fix aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,14 +94,26 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Sets paperclip to upload to amazon S3
-  # config.paperclip_defaults = {
-  #   storage: :s3,
-  #   s3_credentials: {
-  #     bucket: ENV.fetch('S3_BUCKET_NAME'),
-  #     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-  #     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-  #     s3_region: ENV.fetch('AWS_REGION'),
-  #   }
-  # }
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 
 end
+
+# sets paperclip to upload images to Amazon S3
+  # Variables directed to heroku via the command line for pw etc
+  # config.paperclip_defaults = {
+  # storage: :s3,
+  # s3_host_name: 's3-eu-west-1.amazonaws.com',
+  # s3_credentials: {
+  #   bucket: ENV.fetch('S3_BUCKET_NAME'),
+  #   access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+  #   secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+  #   s3_region: ENV.fetch('AWS_REGION'),
+  # }
